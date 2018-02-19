8d0229d4345db333e74ffa7e0480c3ebc1db3b55
Bishesh Katwal <kbishesh2072@gmail.com>
2018-02-19 10:54:55 +0545

    calculating area and parameter of square

---
+++ question_1.py
@@ -0,0 +1,21 @@
+#Q1. Create a SquareGeometry class which takes in length as initialize parameter.
+#  Make two methods getArea and getPerimeter inside this class.
+#  Which when invoked returns area and perimeter of each square instance.
+
+class SquareGeometry:
+    def __init__(self,length):
+        self.length=length
+
+
+    def getArea(self):
+        return self.length*self.length
+
+
+    def getPerimeter(self):
+        return 4*self.length
+
+l= SquareGeometry(4)
+print(l.getArea())
+print(l.getPerimeter())
+
+
